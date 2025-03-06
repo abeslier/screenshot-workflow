acfc619973d3a92c7592786e1ca821b5477181ca
abeslier <Adrien.Beslier@ulb.be>
2025-03-06 16:17:38 +0100

    script output_path arg

--- screenshot.py
+++ screenshot.py
@@ -2,13 +2,13 @@ import sys, os
 from selenium import webdriver
 
 
-def screenshot(url):
+def screenshot(input_path, output_path):
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     try:
-        driver.get(url)
-        driver.save_screenshot("screenshot.png")
+        driver.get(input_path)
+        driver.save_screenshot(output_path)
     except Exception as e:
         print(e)
     finally:
@@ -16,4 +16,4 @@ def screenshot(url):
 
 
 if __name__ == "__main__":
-    screenshot(f"file:///{os.path.abspath(sys.argv[1])}")
+    screenshot(f"file:///{os.path.abspath(sys.argv[1])}", sys.argv[2])
